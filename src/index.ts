3f09589ede6b8090ca6a39cd2ff85d672072d6e0
James Chong <wei3hua2@gmail.com>
2018-07-16 21:53:55 +0800

    update packages

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-/**
- * @module replace-string
- */
-
 const replaceString = require('replace-string');
 import {RpsContext,RpsModule,rpsAction} from 'rpscript-interface';
 
+/** Replace String module
+ * @namespace replace-string
+*/
 @RpsModule("replace-string")
 export default class RPSReplaceString {
 
