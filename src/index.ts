1f4c03fe9af90597376cf437e0e376843569bf94
James Chong <wei3hua2@gmail.com>
2018-07-17 14:40:17 +0800

    update documentation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
 const replaceString = require('replace-string');
 import {RpsContext,RpsModule,rpsAction} from 'rpscript-interface';
 
-/** Replace String module
- * @namespace replace-string
+/** A convenient way to replace string
+ * @see {@link https://www.npmjs.com/package/replace-string|Replace-String}
+ * @namespace Replace-String
 */
 @RpsModule("replace-string")
 export default class RPSReplaceString {
 
+    /**
+ * @function replace-content
+ * @memberof Replace-String
+ * @example
+ * ;Print 'hello yo world yo'
+ * replace-content "hello hey world hey" "hey" "yo"
+ * @param {string} content count or melody string.
+ * @param {string} needle count or melody string.
+ * @param {string} replacement count or melody string.
+ * @param {number} fromIndex optional --fromIndex=5
+ * @returns {void} 
+ * @summary Replace string content
+ * 
+ * @see {@link https://www.npmjs.com/package/replace-string}
+ * 
+*/
   @rpsAction({verbName:'replace-content'})
   async replaceString (ctx:RpsContext,opts:Object, content:string,needle:string,replacement:string) : Promise<string>{
     return replaceString(content,needle,replacement,opts);
